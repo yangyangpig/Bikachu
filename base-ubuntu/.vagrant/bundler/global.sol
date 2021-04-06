0056ba100262f1750c16fc4c325aae05d750ae03
FuhongLiu <liufuhong@52tt.com>
2021-04-06 20:39:35 +0800

    配置nginx-controller作为harbor的ingress

---
+++ base-ubuntu/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"0d42b708e994293a679a345bb098ddfe66593e7c9c4e683516fa2f5d83cfda32","vagrant_version":"2.2.10"}
