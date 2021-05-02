60e16ee6b4945f0d97aea71bb1e852bbd52ab836
FuhongLiu <liufuhong@52tt.com>
2021-05-02 10:59:26 +0800

    修复由于kubeadm init时候设置cidr的po可用ip段和master节点ip冲突造成无法安装问题

--- k8s-cluster/data/image.sh
+++ k8s-cluster/data/image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 images=(
-    kube-apiserver:v1.20.5
-    kube-controller-manager:v1.20.5
-    kube-scheduler:v1.20.5
-    kube-proxy:v1.20.5
-    pause:3.2
+    kube-apiserver:v1.21.0
+    kube-controller-manager:v1.21.0
+    kube-scheduler:v1.21.0
+    kube-proxy:v1.21.0
+    pause:3.4.1
     etcd:3.4.13-0
-    coredns:1.7.0
+    #coredns:v1.8.0
 )
 
 for imageName in ${images[@]} ; do
@@ -15,6 +15,11 @@ for imageName in ${images[@]} ; do
     docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
     docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
 done
+
+# 由于最新版的kubernetes:v1.21.0需要coredns:v1.8.0，但是registry.cn-hangzhou.aliyuncs.com/google_containers没有，只能从docker上下载再改tag
+docker pull louwy001/coredns-coredns:v1.8.0
+docker tag louwy001/coredns-coredns:v1.8.0 k8s.gcr.io/coredns/coredns:v1.8.0
+
 
 # 关于cni的还需要下载几个镜像
  # docker pull calico/node:v3.18.1
